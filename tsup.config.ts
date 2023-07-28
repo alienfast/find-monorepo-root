78b18bbb594cb026725f2b89436c8dd1c37effe3
Kevin Ross <kevin.ross@alienfast.com>
2023-07-28 16:36:55 -0500

    add bin entrypoint and correct bin change from auto setup

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'tsup'
 
 // this is meant to be executed cwd in the package dir, but shared amongst all via `yarn tsup --config ../../tsup.config.ts`
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/bin/findMonorepoRoot.ts'],
   sourcemap: true,
   clean: true,
   dts: true,
   format: ['esm'],
-  minify: true,
+  minify: false,
 })
